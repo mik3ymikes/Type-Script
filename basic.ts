c5b59851740c51f932aa9d6d0b8c6a9c05dc187c
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-09-24 20:15:58 -0500

    working on typescript lesosns

---
+++ basic.ts
@@ -0,0 +1,31 @@
+let age:number;
+
+age=12
+
+let userName:string
+
+userName="Max"
+
+
+let isInsturctor:boolean
+
+isInsturctor=true
+
+let hobbies:string[]
+
+hobbies=["sports", "cooking"]
+
+let person:{
+    name:string;
+    age:number
+}
+
+person={
+    name:"Max",
+    age:32
+}
+
+// person={
+//     isEmployee:true
+// }
+//wont work cus wrong type
